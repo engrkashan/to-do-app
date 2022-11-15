e923231c7cb806168f1ffac60c0f08a7208b75c8
Kashee Umar <kshfkhl@gmail.com>
2022-11-15 19:33:46 +0500

    new updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import "./App.css";
+import _ from "lodash";
+import { v4 } from "uuid";
+
+const item = {
+  id: v4(),
+  name: "Clean the room and sleep",
+};
+
+const item2 = {
+  id: v4(),
+  name: "Make a website",
+};
 
 function App() {
+  const [state, setState] = useState({
+    "to-do": {
+      title: "Todo",
+      items: [item],
+    },
+    "in-progress": {
+      title: "In-progress",
+      items: [item2],
+    },
+    done: {
+      title: "Completed",
+      items: [],
+    },
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext onDragEnd={(e) => console.log(e)}>
+        {_.map(state, (data, key) => {
+          return (
+            <div key={key} className="column">
+              <h3>{data.title}</h3>
+              <Droppable droppableId={key}>
+                {(provided) => {
+                  return (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      className={"droppable-col"}
+                    >
+                      {data.items.map((el: T, index: number) => {
+                        return (
+                          <Draggable
+                            key={el.id}
+                            index={index}
+                            droppableId={el.id}
+                          >
+                            {(provided) => {
+                              return (
+                                <div
+                                  ref={provided.innerRef}
+                                  {...provided.droppableProps}
+                                  {...provided.dragHandleProps}
+                                >
+                                  {el.name}
+                                </div>
+                              );
+                            }}
+                          </Draggable>
+                        );
+                      })}
+                    </div>
+                  );
+                }}
+              </Droppable>
+            </div>
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
